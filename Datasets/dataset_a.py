74ffe35fb53b9ea1ca86d42793eac73c71016468
Lucas Teixeira <lucasteixeirag11@gmail.com>
2017-08-17 17:13:50 -0300

    Including optimizer in the new model

--- Datasets/dataset_a.py
+++ Datasets/dataset_a.py
@@ -4,6 +4,6 @@ class DatasetA(dataset.Dataset):
     def __init__(self):
         parameters_list = ["batch_size", "batch_size_val", "variable_names"]
         self.open_config(parameters_list)
-    def next_batch(self, batch_size):
-        return "usa o dataset A com batch size " + str(batch_size)
+    def next_batch_train(self):
+        return "usa o dataset A com batch size " + str(self.config_dict["batch_size"]), "ground_truth"
     
--- Datasets/dataset_b.py
+++ Datasets/dataset_b.py
@@ -5,4 +5,4 @@ class DatasetB(dataset.Dataset):
     def next_batch_train(self, batch_size):
         return "usa o dataset B com batch size " + str(batch_size)
     def next_batch_validation(self, batch_size):
-            return "usa o dataset B com batch size " + str(batch_size)
+        return "usa o dataset B com batch size " + str(batch_size)
