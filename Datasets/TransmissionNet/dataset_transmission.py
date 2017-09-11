a685d9cff8a36549c7a693fdf9e19b92848065d6
RaelCappra <rael.cappra@gmail.com>
2017-09-11 15:46:21 -0300

    DatasetTransmission next_batch adapted for new pipeline (TODO: solve batch size problem)

--- Datasets/TransmissionNet/dataset_transmission.py
+++ Datasets/TransmissionNet/dataset_transmission.py
@@ -10,7 +10,7 @@ import dataset
 
 class DatasetTransmission(dataset.Dataset):
     def __init__(self):
-        parameters_list = ["tfr_path", "input_size", "output_size", "turbidity_path",
+        parameters_list = ["train_path", "test_path", "input_size", "output_size", "turbidity_path",
                            "turbidity_size", "patch_size", "trans_minval", "trans_maxval"]
         self.config_dict = self.open_config(parameters_list)
         self.batch_size = self.config_dict["batch_size"]
@@ -19,11 +19,10 @@ class DatasetTransmission(dataset.Dataset):
         self.input_size_prod = self.input_size[0] * self.input_size[1] * self.input_size[2]
         self.output_size = self.config_dict["output_size"]
         self.output_size_prod = self.output_size[0] * self.output_size[1] * self.output_size[2]
-        self.tfr_path = self.config_dict["tfr_path"]
-        self.train_file = [self.tfr_path + f for f in os.listdir(self.tfr_path)]
-        self.num_file = len(self.train_file)
-        self.validation_file = [self.tfr_path + f for f in os.listdir(self.tfr_path)]
-        self.num_files_val = len(self.validation_file)
+        self.train_path = self.config_dict["train_path"]
+        self.train_file = [self.train_path + f for f in os.listdir(self.train_path)]
+        self.test_path = self.config_dict["test_path"]
+        self.test_file = [self.test_path + f for f in os.listdir(self.test_path)]
         #Simulator attributes
         self.turbidity_path = self.config_dict["turbidity_path"]
         self.turbidity_size = tuple(self.config_dict["turbidity_size"])
@@ -45,7 +44,24 @@ class DatasetTransmission(dataset.Dataset):
 
         return image
 
-    def next_batch_train(self):
+    def _parse_function(self, record):
+        features = {"image_raw": tf.FixedLenFeature((), tf.string),}
+                    
+        parsed_features = tf.parse_single_example(record, features)
+        image = tf.decode_raw(parsed_features['image_raw'], tf.uint8)
+        image.set_shape([self.input_size_prod])
+        image = tf.cast(image, tf.float32) * (1. / 255)
+        image = tf.reshape(image, self.input_size)
+        # depth = tf.cast(depth, tf.float32)
+        return image
+
+    def random_transmissions(self, batch_size):
+        minval = self.config_dict["trans_minval"]
+        maxval = self.config_dict["trans_maxval"]
+        transmissions = tf.random_uniform([batch_size], minval=minval, maxval=maxval)
+        return transmissions
+
+    def next_batch_train(self, initial_step):
         """
         args:
             batch_size:
@@ -59,21 +75,18 @@ class DatasetTransmission(dataset.Dataset):
                 transmissions is a float tensor with shape [batch size]
         """
 
-        filename = self.train_file
-        print(filename)
+        filenames = self.train_file
 
-        filename_queue = tf.train.string_input_producer(
-            filename, num_epochs=self.config_dict["num_epochs"])
+        dataset = tf.contrib.data.TFRecordDataset(filenames)
+        dataset = dataset.map(self._parse_function)  # Parse the record into tensors.
+        dataset = dataset.shuffle(buffer_size=3000)
+        dataset = dataset.batch(self.config_dict["batch_size"])
+        dataset = dataset.repeat(self.config_dict["num_epochs"])  # Repeat the input.
+        dataset = dataset.skip(initial_step)
 
-        image = self.read_and_decode(filename_queue)
+        iterator = dataset.make_one_shot_iterator()
 
-        images = tf.train.shuffle_batch(
-            [image], batch_size=self.config_dict["batch_size"],
-            num_threads=self.config_dict["num_threads"],
-            capacity=100+ 3 * self.config_dict["batch_size"],
-            min_after_dequeue=100
-            )
-        images = tf.reshape(images, [self.batch_size] + self.input_size)
+        images = iterator.get_next()
 
         size_x = self.config_dict['patch_size'][0]
         size_y = self.config_dict['patch_size'][1]
@@ -81,11 +94,40 @@ class DatasetTransmission(dataset.Dataset):
         offset_y = random.randint(0, self.input_size[0] - size_y - 1)
 
         images = images[:, offset_x:offset_x + size_x, offset_y:offset_y+size_y]
-        minval = self.config_dict["trans_minval"]
-        maxval = self.config_dict["trans_maxval"]
-        transmissions = tf.random_uniform([self.batch_size], minval=minval, maxval=maxval)
+        transmissions = self.random_transmissions(self.batch_size)
         images = simulator.applyTurbidityTransmission(images, self.binf, transmissions)
-        print(images.shape)
-        print(transmissions.shape)
         tf.summary.image("image", images)
         return images, transmissions
+
+    #TODO: resolver o problema do batch size variavel
+    def next_batch_test(self):
+        """
+        returns:
+            a tuple(image, transmissions) where:
+                image is a float tensor with shape [batch size] + input_size
+                transmissions is a float tensor with shape [batch size]
+        """
+
+        filenames = self.test_file
+
+        dataset = tf.contrib.data.TFRecordDataset(filenames)
+        dataset = dataset.map(self._parse_function)  # Parse the record into tensors.
+        dataset = dataset.batch(self.config_dict["batch_size"])
+        iterator = dataset.make_initializable_iterator()
+        initializer = iterator.initializer
+
+        images = iterator.get_next()
+
+        size_x = self.config_dict['patch_size'][0]
+        size_y = self.config_dict['patch_size'][1]
+        offset_x = random.randint(0, self.input_size[0] - size_x - 1)
+        offset_y = random.randint(0, self.input_size[0] - size_y - 1)
+
+        images = images[:, offset_x:offset_x + size_x, offset_y:offset_y+size_y]
+        # depths = tf.reshape(depths, [self.batch_size] + self.output_size)
+        # images = tf.reshape(images, [self.batch_size] + self.input_size)
+
+        transmissions = self.random_transmissions(self.batch_size)
+        images = simulator.applyTurbidityTransmission(images, self.binf, transmissions)
+        tf.summary.image("image", images)
+        return images, transmissions, initializer
--- png_to_tfrecords.py
+++ png_to_tfrecords.py
@@ -20,47 +20,47 @@ WRITER_TRAIN = tf.python_io.TFRecordWriter(TF_RECORDS_TRAIN_FILENAME)
 WRITER_VALIDATION = tf.python_io.TFRecordWriter(TF_RECORDS_VALIDATION_FILENAME)
 
 IMAGE_PATH = 'Datasets/Data/nyu/images/'
-DEPTH_PATH = 'Datasets/Data/nyu/depths/'
+#DEPTH_PATH = 'Datasets/Data/nyu/depths/'
 
 IMAGES = sorted([IMAGE_PATH + f for f in os.listdir(IMAGE_PATH)])
-DEPTHS = sorted([DEPTH_PATH + f for f in os.listdir(DEPTH_PATH)])
+#DEPTHS = sorted([DEPTH_PATH + f for f in os.listdir(DEPTH_PATH)])
 
 train_length = int(0.9 * len(IMAGES))
 validation_length = len(IMAGES) - train_length
 images_train = IMAGES[:train_length]
-depths_train = DEPTHS[:train_length]
+#depths_train = DEPTHS[:train_length]
 
 images_validation = IMAGES[-validation_length:]
-depths_validation = DEPTHS[-validation_length:]
+#depths_validation = DEPTHS[-validation_length:]
 
-FILENAME_TRAIN_PAIRS = zip(images_train, depths_train)
-FILENAME_VALIDATION_PAIRS = zip(images_validation, depths_validation)
+#FILENAME_TRAIN_PAIRS = zip(images_train, depths_train)
+#FILENAME_VALIDATION_PAIRS = zip(images_validation, depths_validation)
 
-for img_path, depth_path in FILENAME_TRAIN_PAIRS:
+for img_path in images_train:#, depth_path in FILENAME_TRAIN_PAIRS:
     img = np.array(Image.open(img_path))
-    depth = np.array(Image.open(depth_path)) * (255.0/65535.0)
-    depth = depth.astype(np.uint8)
+    #depth = np.array(Image.open(depth_path)) * (255.0/65535.0)
+    #depth = depth.astype(np.uint8)
     image_raw = img.tostring()
-    depth_raw = depth.tostring()
+    #depth_raw = depth.tostring()
 
     example = tf.train.Example(features=tf.train.Features(feature={
         'image_raw': _bytes_feature(image_raw),
-        'depth_raw': _bytes_feature(depth_raw)}))
+        }))#    'depth_raw': _bytes_feature(depth_raw)}))
     WRITER_TRAIN.write(example.SerializeToString())
 
 WRITER_TRAIN.close()
 
 
-for img_path, depth_path in FILENAME_VALIDATION_PAIRS:
+for img_path in images_validation:#, depth_path in FILENAME_VALIDATION_PAIRS:
     img = np.array(Image.open(img_path))
-    depth = np.array(Image.open(depth_path)) * (255.0/65535.0)
-    depth = depth.astype(np.uint8)
+    #depth = np.array(Image.open(depth_path)) * (255.0/65535.0)
+    #depth = depth.astype(np.uint8)
     image_raw = img.tostring()
-    depth_raw = depth.tostring()
+    #depth_raw = depth.tostring()
 
     example = tf.train.Example(features=tf.train.Features(feature={
         'image_raw': _bytes_feature(image_raw),
-        'depth_raw': _bytes_feature(depth_raw)}))
+        }))#'depth_raw': _bytes_feature(depth_raw)}))
     WRITER_VALIDATION.write(example.SerializeToString())
 
 WRITER_VALIDATION.close()
